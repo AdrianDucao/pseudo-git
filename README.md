f667304906801ee461109905aaed713ea09e82ca
AdrianDucao <adrianducao@gmail.com>
2020-08-29 11:15:15 +0800

    start

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ A proof of concept git like version control. Digging deep on how it works
 ### To do:
 - [ ] initite using something like e.g. python3 pseudo.py -edit file.txt
 - [ ] track changes by comparing checksums and saving every file whenever we do, e.g. python3 pseudo.py -save file.txt, on a local database, might use sql lite or database.c
-- [ ] need to display a comprehensive history of every -save just like in the real git where you can view the previous commits and the changes done with the file.  
+- [ ] need to display a comprehensive history of every -save just like in the real git where you can view the previous commits and the changes done with the file. 
+
+### commands
+* pseudo.py -save /filename	: much like commit -a	
+* pseudo.py -edit /filename	: much like when editing a file 
+* pseudo.py -history		: much like viewing commit history
--- pseudo.py
+++ pseudo.py
@@ -9,3 +9,45 @@ currentDirectory = os.getcwd()
 dir = os.path.join(currentDirectory,".pseudo")
 if not os.path.exists(dir):
     os.mkdir(dir)
+
+class Checksum:
+    
+    def __init__(self, currentFile, prevFile):
+        self.currentFile = filename #most recent -save
+        self.prevFile = filename #saved object from database
+
+    def compare(self):
+        md5_hash = hashlib.md5()
+        a_file = open("test.txt", "rb")
+        content = a_file.read()
+        md5_hash.update(content)
+
+        digest = md5_hash.hexdigest()
+        print(digest)
+
+class SaveObject:
+
+class ObjectHistory:
+
+
+
+if __name__ == '__main__':
+    
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-action', required=True)
+    parser.add_argument('-keyfile')
+
+    
+    args = parser.parse_args()
+    action = args.action.lower()
+    keyfile = args.keyfile
+
+    #
+
+    if action == 'edit':
+        
+    elif action == 'save':
+        
+    elif action == 'history':
